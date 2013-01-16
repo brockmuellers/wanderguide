0ac2b1dca9d4a5a7e659a02ed364cd6b2092f1b6
Cristina <clozano@mit.edu>
2013-01-15 20:48:14 -0500

    mockups done

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 =====
 
 Vacation website for the MIT 6.470 Competition
+
+Acknowledgements:
+"Font Awesome by Dave Gandy - http://fortawesome.github.com/Font-Awesome"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :password_hash, :salt, :username
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :username
+  
+  validate :email_must_be_unique
+  validates :password, :presence => true, :on => :create
+  validates :email, :name, :presence => true
+
+  # Validation to ensure emails are unique across both Merchants and Customers.
+  def email_must_be_unique
+    if ((Organization.where(['email = ?', self.email]).count > 0) or
+        (Volunteer.where(['email = ? AND id <> ?', self.email, self.id]).count > 0))
+
+      errors.add(:email, "is already taken")
+    end
+  end
+  
 end
